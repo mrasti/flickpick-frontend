eb87dd5d3ee4a643faabd71f0cae428d6464b882
DCartist <pbannerman13@gmail.com>
2019-06-25 16:37:23 -0400

    Connecting to DB working on Connecting to Api

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import {Card, Button} from 'react-bootstrap';
+import ImgPlace from './250x200.jpg';
 
 class Movie extends Component {
+    
     render() {
         return (
             <div>
-                
+                <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={ImgPlace} />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
             </div>
         );
     }
