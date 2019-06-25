ffe7f4823ba13550877f56b5f0decee29339cc10
DCartist <pbannerman13@gmail.com>
2019-06-25 11:19:09 -0400

    Adding Router dependencies

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import 'react-bootstrap';
 
 class Navigation extends Component {
     render() {
         return (
             <div>
-                
+                {/* Start of Navigation */}
+<p>Navigation</p>
+
+                {/* End of Navigation */}
             </div>
         );
     }
