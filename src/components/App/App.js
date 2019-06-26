cdf361b64a8a76ccfc77a2acf8862701a6b1eccb
Shane Vann-Shirley <ashanev@gmail.com>
2019-06-26 08:28:26 -0400

    began work on login component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,23 @@
 import React, { Component } from "react";
-import './App.css'
-import Home from '../Home/Home';
-import Movies from '../MovieList/MovieList'
-import Navigation from '../Navigation/Navigation'
-import {Route, Link, Switch, Redirect} from "react-router-dom";
-import 'react-bootstrap';
+import "./App.css";
+import Home from "../Home/Home";
+import Movies from "../MovieList/MovieList";
+import Login from "../Login/Login";
+import Navigation from "../Navigation/Navigation";
+import { Route, Link, Switch, Redirect } from "react-router-dom";
+import "react-bootstrap";
 
 class App extends Component {
-  render(){
+  render() {
     return (
       <div>
-        <Navigation></Navigation> 
+        <Navigation />
+        <Login />
         {/* Navigation bar (Responsive) */}
-        <Route path="/" exact component={Home}/>
-        <Route path="/movies" exact component={Movies}/>
+        <Route path="/" exact component={Home} />
+        <Route path="/movies" exact component={Movies} />
       </div>
     );
   }
-
 }
 export default App;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Login extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <form>
+          <div>
+            <p>Email</p>
+            <input
+              className="input-field"
+              type="text"
+              name="email"
+              onChange={this.props.handleInput}
+              autoComplete="off"
+            />
+          </div>
+          <div>
+            <p>Password</p>
+            <input
+              className="input-field"
+              type="password"
+              name="password"
+              autoComplete="off"
+              onChange={this.props.handleInput}
+            />
+          </div>
+          <input
+            className="submit-button"
+            type="submit"
+            value="Submit"
+            onClick={this.props.handleLogIn}
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Login;
