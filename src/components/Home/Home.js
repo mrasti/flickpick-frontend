50f47682e794abb7ab03d84c1e7b02717f7f38fe
Shane Vann-Shirley <ashanev@gmail.com>
2019-06-26 18:12:11 -0400

    removed show movies from home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,53 +1,55 @@
-import React, { Component } from 'react';
-import Movie from '../Movie/Movie'
-import Axios from 'axios';
-import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed'
+import React, { Component } from "react";
+import Movie from "../Movie/Movie";
+import Axios from "axios";
+import ResponsiveEmbed from "react-bootstrap/ResponsiveEmbed";
 
 class Home extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            movieInfo:[],
-           
-        }
-    }
-    componentDidMount() {
-        const url = "http://localhost:3000/api/movies/"
-        Axios.get(url)
-        .then(res=>{
-            console.log(res.data.results)
-            let results = res.data.results;
-            this.setState(
-                prevState => ({
-                    movieInfo: results
-                })
-              );
-        // console.log(movieInfo)
-        })
-    }
-    render() {
-        let list = this.state.movieInfo.map((item,index) =>{
-            console.log(item)
-       return (
+  constructor(props) {
+    super(props);
+    this.state = {
+      movieInfo: []
+    };
+  }
+  componentDidMount() {
+    const url = "http://localhost:3000/api/movies/";
+    Axios.get(url).then(res => {
+      console.log(res.data.results);
+      let results = res.data.results;
+      this.setState(prevState => ({
+        movieInfo: results
+      }));
+      // console.log(movieInfo)
+    });
+  }
+  render() {
+    let list = this.state.movieInfo.map((item, index) => {
+      console.log(item);
+      return (
         <div key={index}>
-        <Movie title={item.title} overview={item.overview} posterImage={item.posterImage} id={item.id}></Movie>
+          <Movie
+            title={item.title}
+            overview={item.overview}
+            posterImage={item.posterImage}
+            id={item.id}
+          />
         </div>
-       )
-       
-        })
-        return (
-            <div className="homeTop">
-                <div style={{ width: '100%', height: 'auto' }}>
-  <ResponsiveEmbed aspect="a16by9">
-    <embed type="image/svg+xml" src="https://player.vimeo.com/video/344644642" />
-  </ResponsiveEmbed>
-</div>
+      );
+    });
+    return (
+      <div className="homeTop">
+        <div style={{ width: "100%", height: "auto" }}>
+          <ResponsiveEmbed aspect="a16by9">
+            <embed
+              type="image/svg+xml"
+              src="https://player.vimeo.com/video/344644642"
+            />
+          </ResponsiveEmbed>
+        </div>
 
-                {list}
-                
-            </div>
-        );
-    }
+        {/* {list} */}
+      </div>
+    );
+  }
 }
 
 export default Home;
