533c4411cf599bf679e8e0d6116056efaa88ebe3
DCartist <pbannerman13@gmail.com>
2019-06-27 05:46:04 -0400

    genre list pulling testing

--- src/components/GenreList/GenreList.js
+++ src/components/GenreList/GenreList.js
@@ -1,13 +1,75 @@
 import React, { Component } from 'react';
+import Axios from 'axios'
+import Movie from "../Movie/Movie";
 
 class GenreList extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
+    constructor(props){
+        super(props)
+        this.state = {
+            genre: [],
+            movieRes:[]
+        }
     }
+    componentDidMount() {
+        const movieurl = "http://localhost:3000/api/movies/";
+        Axios.get(movieurl).then(
+            res=>{
+                console.log(res)
+                let movieresults = res.data.results;
+                this.setState(prevState => ({
+                    movieRes: movieresults
+                }))
+            }
+        )
+        const url = "http://localhost:3000/api/genre/";
+        Axios.get(url).then(res => {
+          console.log(res.data);
+          console.log(typeof res.data);
+          let results = res.data;
+          this.setState(prevState => ({
+            genre: results
+          }));
+        });
+        //end of get Genres from Axios
+        
+      }
+      render() {
+        let list = this.state.genre.map(item => {
+                let movielist = this.state.movieRes.map((movieItem, index) => {
+                    let findMovieImage = movieItem.genre_ids.map(genreResult =>{
+                       if(16 === genreResult){
+                        console.log(`${movieItem.id} ${movieItem.title}<< Item Name GenreREs ${genreResult} `)
+                        return (
+                            <div key={index}>
+                                movie
+                                {movieItem.title}
+                                {/* <Movie
+            title={movieItem.title}
+            overview={movieItem.overview}
+            posterImage={movieItem.posterImage}
+            id={movieItem.id}
+            userInfo={this.props.userInfo}
+          /> */}
+                            </div>
+                        );
+                       }
+                    })
+                })
+        //   return (
+        //     <div key={index}>
+
+        //         <p>{item.name}</p>
+        //     </div>
+        //   );
+        });
+
+
+        return (
+        <div className="columns">
+        {list}
+        this
+        </div>)
+      }
 }
 
 export default GenreList;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Movie from "../Movie/Movie";
 import Axios from "axios";
 import ResponsiveEmbed from "react-bootstrap/ResponsiveEmbed";
+import Genre from '../GenreList/GenreList'
 
 class Home extends Component {
   constructor(props) {
@@ -10,41 +11,49 @@ class Home extends Component {
       movieInfo: []
     };
   }
-  componentDidMount() {
-    const url = "http://localhost:3000/api/movies/";
-    Axios.get(url).then(res => {
-      console.log(res.data.results);
-      let results = res.data.results;
-      this.setState(prevState => ({
-        movieInfo: results
-      }));
-      // console.log(movieInfo)
-    });
-  }
+//   componentDidMount() {
+//     const url = "http://localhost:3000/api/movies/";
+//     Axios.get(url).then(res => {
+//     //   console.log(res.data.results);
+//       let results = res.data.results;
+//       this.setState(prevState => ({
+//         movieInfo: results
+//       }));
+//       // console.log(movieInfo)
+//     });
+//   }
   render() {
-    let list = this.state.movieInfo.map((item, index) => {
-      console.log(item);
-      return (
-        <div key={index}>
-          <Movie
-            title={item.title}
-            overview={item.overview}
-            posterImage={item.posterImage}
-            id={item.id}
-          />
-        </div>
-      );
-    });
+    // let list = this.state.movieInfo.map((item, index) => {
+    //   console.log(item);
+    //   return (
+    //     <div key={index}>
+    //       <Movie
+    //         title={item.title}
+    //         overview={item.overview}
+    //         posterImage={item.posterImage}
+    //         id={item.id}
+    //       />
+    //     </div>
+    //   );
+    // });
     return (
       <div className="homeTop">
-        <div style={{ width: "100%", height: "auto" }}>
+          TEST
+          <Genre></Genre>
+          {/* <ResponsiveEmbed>
+            <embed
+            //   type="image/svg+xml"
+              src="https://player.vimeo.com/video/344644642"
+            />
+          </ResponsiveEmbed> */}
+        {/* <div style={{ width: "100%", height: "auto" }}>
           <ResponsiveEmbed aspect="a16by9">
             <embed
               type="image/svg+xml"
               src="https://player.vimeo.com/video/344644642"
             />
           </ResponsiveEmbed>
-        </div>
+        </div> */}
 
         {/* {list} */}
       </div>
