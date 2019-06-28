8657f7221dc9f200150d81befce46b7e1c6d3e23
DCartist <pbannerman13@gmail.com>
2019-06-28 09:46:58 -0400

    Finishing About Section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import ProtoImage1 from "./prototype.jpg";
+import ProtoImage2 from "./prototype1.jpg";
+import ModelImage from "./flickpick_erd.png"
+import FlixPick from "../Navigation/flickpick.png"
 
 class About extends Component {
     render() {
@@ -6,17 +10,34 @@ class About extends Component {
             <div className="About">
             
             <h2>Abo<span>ut</span></h2>
-            <h4>Welcome to FlickPick</h4>
-            <h3>This App was built by:</h3>
-            <span>Shane Vann-Shirley, Paula Bannerman, Melika Rasti</span>
+            <img src={FlixPick}></img>
+            <h3 className="Centered">This App was built by:</h3>
+            <p className="Centered">Shane Vann-Shirley, Paula Bannerman, Melika Rasti</p>
             <p>Our goal is to create a movie app that allows a user to log in, search for movies by various identifiers, and then store movies in a to-do list</p>
-            <p>Models Used</p>
+            <h3>Technolo<span>gy</span> Us<span>ed</span></h3>
             <ul>
-                <h3>User</h3>
+                <li>Express</li>
+                <li>CORS</li>
+                <li>ReactJS/ ReactDOM</li>
+                <li>NodeJS</li>
+                <li>Mongoose</li>
+                <li>MongoDB</li>
+                <li>CSS</li>
+                <li>HTML</li>
+            </ul>
+            <h3>Re<span>po</span> Li<span>st</span></h3>
+            <ul>
+                <li><a href="https://github.com/ashanevs/flickpick" target="_blank">FlickPick React App</a></li>
+                <li><a href="https://github.com/ashanevs/flickpick-api" target="_blank">FlickPick App Api</a></li>
+            </ul>
+            <h3>Mode<span>ls</span> Us<span>ed</span></h3>
+            <ul>
+                <h4>User</h4>
                 <li>name: String,</li>
                 <li>password: String,</li>
                 <li>listOfMovies: []</li>
-                <h3>Movie</h3>
+                <p></p>
+                <h4>Movie</h4>
                 <li>id: Number,</li>
                 <li>title: String,</li>
                 <li>overview: String,</li>
@@ -32,10 +53,23 @@ class About extends Component {
                 <li>genre_ids: Array,</li>
                 <li>cast_crew: [],</li>
                 <li>genres: []</li>
-  
+                <p></p>
+            <h4>Genre</h4>
+            <li>id: Number,</li>
+            <li>name: String</li>
             </ul>
+            <img src={ModelImage}></img>
+            <h3>Bon<span>us</span> Objectiv<span>es</span></h3>
+            <p>Adding authentication for users, embedding videos</p>
+            <h3>Distributi<span>on</span> of Wo<span>rk</span></h3>
+            <p>
+            Melika/Shane - back end; Melika, Shane and Paula - front end
+            </p>
 
+            <h3>WireFra<span>me</span> of Desi<span>gn</span></h3>
 
+            <img src={ProtoImage1}></img>
+            <img src={ProtoImage2}></img>
             </div>
         );
     }
